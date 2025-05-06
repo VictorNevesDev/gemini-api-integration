a95b495691d59691665ea97fc1bccf9ff382c405
victor neves <victorneves@weg.net>
2025-05-06 15:47:14 -0300

    Add Question

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ dotenv.config();
 
 import { GoogleGenAI } from "@google/genai";
 
+import { makeQuestion } from "./question.js";
+
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 async function main() {
+
+  let prompt = "Only answer geography related questions. If the questions is not related to geography, tell the user that you can't answer the question" +
+  "The user asked: "
+
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: "Where Brazil is located?",
+    contents: prompt + await makeQuestion("Where Philipines is located? "),
   });
   console.log(response.text);
 }
